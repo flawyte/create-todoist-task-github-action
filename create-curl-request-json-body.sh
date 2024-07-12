87cfe73ee2139e608a81167fd9ef0b4cc917ae1d
Mickaël Allonneau <mickael.allonneau.pro@gmail.com>
2024-07-12 11:31:42 +0200

    feat: add missing parameters

--- create-curl-request-json-body.sh
+++ create-curl-request-json-body.sh
@@ -8,21 +8,64 @@ touch $FILENAME
 # Open JSON
 echo '{' >> $FILENAME
 
-# Handle 'task-name' input
-# echo '{"content": "'"$TASK_NAME"'", "project_id": "'"$TASK_PROJECT_ID"'", "priority": '$TASK_PRIORITY', "section_id": "'"$TASK_SECTION_ID"'"}' >> $FILENAME
+# Handle required inputs
+
 echo '  "content": "'$TASK_NAME'"' >> $FILENAME
 
-# Handle 'task-priority' input
+# Handle optional inputs
+
+if [ -n "$TASK_ASSIGNEE_ID" ]; then
+  echo '  ,"assignee_id": "'$TASK_ASSIGNEE_ID'"' >> $FILENAME
+fi
+
+if [ -n "$TASK_DESCRIPTION" ]; then
+  echo '  ,"description": "'$TASK_DESCRIPTION'"' >> $FILENAME
+fi
+
+if [ -n "$TASK_DUE_DATE" ]; then
+  echo '  ,"due_date": "'$TASK_DUE_DATE'"' >> $FILENAME
+fi
+
+if [ -n "$TASK_DUE_DATETIME" ]; then
+  echo '  ,"due_datetime": "'$TASK_DUE_DATETIME'"' >> $FILENAME
+fi
+
+if [ -n "$TASK_DUE_LANG" ]; then
+  echo '  ,"due_lang": "'$TASK_DUE_LANG'"' >> $FILENAME
+fi
+
+if [ -n "$TASK_DUE_STRING" ]; then
+  echo '  ,"due_string": "'$TASK_DUE_STRING'"' >> $FILENAME
+fi
+
+if [ -n "$TASK_DURATION" ]; then
+  echo '  ,"duration": '$TASK_DURATION >> $FILENAME
+fi
+
+if [ -n "$TASK_DURATION_UNIT" ]; then
+  echo '  ,"duration_unit": "'$TASK_DURATION_UNIT'"' >> $FILENAME
+fi
+
+if [ -n "$TASK_LABELS" ]; then
+  echo '  ,"labels": "'$TASK_LABELS'"' >> $FILENAME
+fi
+
+if [ -n "$TASK_ORDER" ]; then
+  echo '  ,"order": '$TASK_ORDER >> $FILENAME
+fi
+
+if [ -n "$TASK_PARENT_ID" ]; then
+  echo '  ,"parent_id": "'$TASK_PARENT_ID'"' >> $FILENAME
+fi
+
 if [ -n "$TASK_PRIORITY" ]; then
   echo '  ,"priority": '$TASK_PRIORITY >> $FILENAME
 fi
 
-# Handle 'task-project-id' input
 if [ -n "$TASK_PROJECT_ID" ]; then
   echo '  ,"project_id": "'$TASK_PROJECT_ID'"' >> $FILENAME
 fi
 
-# Handle 'task-section-id' input
 if [ -n "$TASK_SECTION_ID" ]; then
   echo '  ,"section_id": "'$TASK_SECTION_ID'"' >> $FILENAME
 fi
